898a441a0f1f18fe64314ec385b979f25fff5779
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-07-09 15:35:45 +0700

    perf(startup): add delays to autostart script to improve performance

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,16 +4,16 @@
 picom --daemon --config $HOME/.config/qtile/scripts/picom.conf &
 
 # Khởi động polkit-gnome-authentication-agent-1
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+(sleep 3 && /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1) &
 
 # Khởi động wired
-/usr/bin/wired &
+(sleep 2 && /usr/bin/wired) &
 
 # Khởi động gnome-keyring-daemon
 eval (gnome-keyring-daemon --start)
 
 # Khởi động nm-applet
-nm-applet &
+(sleep 2 && nm-applet) &
 
 # Khởi động ibus-daemon
 $HOME/.config/qtile/scripts/ibus.sh &
